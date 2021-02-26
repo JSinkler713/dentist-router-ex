159e0e967688c1a32f23cb3605a3cab6b05bb692
James Sinkler <jamessinkler713@gmail.com>
2021-02-26 08:50:24 -0800

    render each page on own route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
+import Contact from './components/Contact'
+import Procedures from './components/Procedures'
 
 function App() {
   return (
     <Router>
       <Route path='/' component={Home} />
+      <Route path='/contact' component={Contact} />
+      <Route path='/procedures' component={Procedures} />
     </Router>
   );
 }
