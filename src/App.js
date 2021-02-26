5df186ebe18e99f5e8b894c2288ca1827d4993f4
James Sinkler <jamessinkler713@gmail.com>
2021-02-26 10:12:53 -0800

    brings in logo, little styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import tooth from './tooth.png'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import Contact from './components/Contact'
@@ -9,17 +10,24 @@ function App() {
   return (
     <Router>
       <nav className='nav'>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/procedures'>Procedures</Link>
-        <Link to='/procedures/root-canal'>Root Canal</Link>
+        <div class='logo'>
+          <Link to='/'>
+            <img src={tooth} style={{width:'50px'}} />
+          </Link>
+        </div>
+        <div class='links'>
+          <Link to='/'>Home</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/procedures'>Procedures</Link>
+          <Link to='/procedures/root-canal'>Root Canal</Link>
+        </div>
       </nav>
-      <>
+      <div className='App'>
         <Route exact path='/' component={Home} />
         <Route path='/contact' component={Contact} />
-        <Route path='/procedures' component={Procedures} />
+        <Route exact path='/procedures' component={Procedures} />
         <Route path='/procedures/:name' render={(props)=> <Procedure {...props}/>} />
-      </>
+      </div>
     </Router>
   );
 }
