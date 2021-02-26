181d6956aec0e4c5f96e2ca55271c57d88e931c8
James Sinkler <jamessinkler713@gmail.com>
2021-02-26 10:12:37 -0800

    adds a procedure component

--- src/components/Procedures.js
+++ src/components/Procedures.js
@@ -1,8 +1,20 @@
-const Procedures = ()=> { 
+const Procedures = (props)=> { 
+    const procedures = [
+    "Deep Cleaning",
+    "Filling",
+    "Crown",
+    "Root Canal",
+    "Oral Surgery"
+  ]
+  const procedureList = procedures.map((singleProcedure, key) => {
+    return <li key={key} >{singleProcedure}</li>
+  })
+
   return ( 
      <div> 
        <h1>Procedures</h1>
        <p>These are my procedures</p>
+       {procedureList}
      </div> 
   ) 
 } 
